d6648f9f2efaf103a74649a56a768c9952c8a63e
ShotgunShaman <127175317+ShotgunShaman@users.noreply.github.com>
2023-03-13 00:18:38 -0700

    Fixed typo

--- game_stats.py
+++ game_stats.py
@@ -1,5 +1,5 @@
 class GameStats:
-    """Track statistics for Poopy's Palace."""
+    """Track statistics for Alien Invasion."""
     def __init__(self, ai_game):
         """Initialize statistics."""
         self.settings = ai_game.settings
